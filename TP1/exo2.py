f78fcf44d8e03c5208b95243209143d490727f7c
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-09-09 20:13:37 +0200

    Met au propre la première partie de l'exo 2

--- TP1/exo2.py
+++ TP1/exo2.py
@@ -5,6 +5,7 @@
 import numbers
 import string
 
+
 def convert_letter_to_number(text: string):
     """
     :param text: the message to be encrypted
@@ -23,19 +24,78 @@ def convert_letter_to_number(text: string):
 
     return positions
 
-def vigenere_cypher(encryption_key:  list[int], message: list[int]):
-    print(len(message))
-    for positon in message:
-        print(positon, encryption_key[1])
+
+def vigenere_cypher():
+
+    message = input('Entrez le texte à chiffrer: ')
+    encryption_key = input('Entrez la clé à utiliser: ')
+
+    message = convert_letter_to_number(message)
+
+    position_of_letter_of_encryption_key = convert_letter_to_number(encryption_key)
+
+    if len(message) > len(position_of_letter_of_encryption_key):
+        multiplicator = (len(message) // len(position_of_letter_of_encryption_key)) + 1
+        encryption_key = position_of_letter_of_encryption_key * multiplicator
+
+
+    counter = 0
+    final_position = []
+    cypher_message = []
+
+    for position in message:
+        if position < 0 or position > 25:
+            final_position.append(position)
+            counter += 1
+        else:
+            final_position.append((position + encryption_key[counter]) % 26)
+            counter += 1
+
+    for position in final_position:
+        cypher_message.append(chr(ord('A') + position))
+
+    return ''.join(cypher_message)
+
+
+def vigenere_decipher():
+
+    message = input('Entrez le texte à déchiffrer: ')
+    decipher_key = input('Entrez la clé à utiliser: ')
+
+    message = convert_letter_to_number(message)
+
+    position_of_letter_of_encryption_key = convert_letter_to_number(decipher_key)
+
+    if len(message) > len(position_of_letter_of_encryption_key):
+        multiplicator = (len(message) // len(position_of_letter_of_encryption_key)) + 1
+        decipher_key = position_of_letter_of_encryption_key * multiplicator
+
+    counter = 0
+    final_position = []
+    decipher_message = []
+
+    for position in message:
+        if position < 0 or position > 25:
+            final_position.append(position)
+            counter += 1
+        else:
+            final_position.append((position - decipher_key[counter]) % 26)
+            counter += 1
+
+    for position in final_position:
+        decipher_message.append(chr(ord('A') + position))
+
+    return ''.join(decipher_message)
+
 
 if __name__ == "__main__":
-    ENCRYPTION_KEY = 'cle'
-    TEXT = "za"
+    choice = input('Souhaitez vous chiffrer ou dechiffrer un message ? (réponse possible : cypher et decipher) ')
 
-    position_of_letter_to_be_cypher = convert_letter_to_number(TEXT)
-    print('message chiffré: %s' % position_of_letter_to_be_cypher)
-
-    position_of_letter_of_encryption_key = convert_letter_to_number(ENCRYPTION_KEY)
-    print('message chiffré: %s' % position_of_letter_of_encryption_key)
-
-    vigenere_cypher(position_of_letter_of_encryption_key, position_of_letter_to_be_cypher)
+    if choice == 'cypher':
+        cypher_message = vigenere_cypher()
+        print('Message chiffré: ', cypher_message)
+    elif choice == 'decipher':
+        decipher_message = vigenere_decipher()
+        print('Message déchiffré: ', decipher_message)
+    else:
+        print()
