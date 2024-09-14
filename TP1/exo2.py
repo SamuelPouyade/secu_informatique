95a87a6b75088f95716b6c9ea0e89618fdeb0039
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-09-14 14:32:20 +0200

    Ajoute la première partie de la question 2 de l'exo 2. Il manque un tri à faire pour réduire le nombre de possibilité de longueur de la clé

--- TP1/exo2.py
+++ TP1/exo2.py
@@ -87,6 +87,60 @@ def vigenere_decipher():
 
     return ''.join(decipher_message)
 
+def get_all_multiple (number: int):
+    half_number = number // 2
+    all_multiple = []
+    all_multiple.append(number)
+
+    # On ajoute un car la boucle for exclu la valeur N
+    for i in range(2, half_number + 1):
+        if number % i == 0:
+            all_multiple.append(i)
+
+    return all_multiple
+
+
+def findAllRepetitions (text: string, nombre_depart_actuel: int, nombre_depart_precedent: int, nombre_arrive: int, taille_possible, mot_precedent: string) :
+    length = len(text)
+
+    if nombre_depart_actuel >= length or nombre_arrive > length:
+        return taille_possible
+
+    if nombre_arrive == length // 2 :
+        if nombre_depart_actuel < length // 2:
+            nombre_depart_actuel += nombre_depart_precedent + 1
+            nombre_arrive = nombre_depart_actuel + 2
+
+    suite = text[nombre_depart_actuel:nombre_arrive]
+
+    if suite == mot_precedent:
+        return taille_possible
+
+    nombre_rep = text.count(suite)
+
+    if nombre_rep <= 1:
+        return findAllRepetitions(text, nombre_depart_actuel, nombre_depart_precedent, nombre_arrive+1, taille_possible, suite)
+    else:
+        all_occurence = []
+        all_difference = []
+        last_occurence = 0
+
+        for i in range(nombre_rep):
+            position = text.find(suite, last_occurence)
+            all_occurence.append(position)
+            # On augmente de 1 juste pour avoir un décalage et trouver la seconde position.
+            last_occurence = position + 1
+
+        for i in range(len(all_occurence) - 1):
+            if all_occurence[i] == 0 :
+                all_difference.append(all_occurence[i + 1])
+            else:
+                all_difference.append(all_occurence[i+1] - all_occurence[i])
+
+
+        all_multiples = get_all_multiple(all_difference[0])
+        taille_possible[suite] = all_multiples
+        return findAllRepetitions(text, nombre_depart_actuel, nombre_depart_precedent + 1, nombre_arrive + 1, taille_possible, suite)
 
 if __name__ == "__main__":
     choice = input('Souhaitez vous chiffrer ou dechiffrer un message ? (réponse possible : cypher et decipher) ')
@@ -97,5 +151,9 @@ if __name__ == "__main__":
     elif choice == 'decipher':
         decipher_message = vigenere_decipher()
         print('Message déchiffré: ', decipher_message)
+    elif choice == 'findAllRepetitions':
+        taille_possible = {}
+        all_position = findAllRepetitions('MFUVAHGUTSGVMFUTUJPPETQSOUCPIFP', 0, 0, 2,  taille_possible, '')
+        print(all_position)
     else:
         print()
