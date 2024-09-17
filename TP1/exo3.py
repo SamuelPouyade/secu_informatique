02cd12577b408b098dc087135e0490b74f14b7ae
Matteo NADLER CAMPOURCY <matteo.nadler.campou@gmail.com>
2024-09-17 15:43:41 +0200

    exo3 : meilleur affichage

--- TP1/exo3.py
+++ TP1/exo3.py
@@ -2,25 +2,29 @@ import hashlib
 import time
 
 
-def text_to_hash(text: str) -> str:
+def text_to_md5(text: str) -> str:
     """
-    Convertit un texte en hash
-    :param text: str
-    :return: str
+    Convertit un texte en hash MD5
+    :param text: le message à convertir
+    :return: le hash MD5
     """
     return hashlib.md5(text.encode()).hexdigest()
 
 
 if __name__ == "__main__":
     hash_to_get = "5a74dd4eef347734c8a0a9a3188abd11"
-    rockyou = open("rockyou.txt", "r", encoding="latin-1")
+    file_name = "rockyou.txt"
+    rockyou = open(file_name, "r", encoding="latin-1")
 
     timer_start = time.time()
 
+    print(f"Recherche du hash {hash_to_get} dans le fichier {file_name}...")
     for password in rockyou:
-        if text_to_hash(password.strip()) == hash_to_get:
+        formatted_password = password.strip()
+        if text_to_md5(formatted_password) == hash_to_get:
             timer_end = time.time()
-            print(f"Le mot de passe est : {password}")
+            print(f"Le mot de passe est : {formatted_password}")
             print(f"Temps d'exécution : {round(timer_end - timer_start, 2)} secondes")
-            break
+            exit(0)
 
+    print("Le mot de passe n'a pas été trouvé")
