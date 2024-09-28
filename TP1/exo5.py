83e7265ab2c486bbd0c13552dbc91be13eb9da04
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-09-28 10:33:47 +0200

    Refacto de l'exo5
    Ce commit permet de refacto l'exercice 5.
    Avec des fonctions pour chaque comportement.
    Le code est plus lisible est sépare mieux les responsabilités.

--- TP1/exo5.py
+++ TP1/exo5.py
@@ -1,10 +1,92 @@
 import sys, socket, csv
+from simple_term_menu import TerminalMenu
+import time
+
+def await_input() -> None:
+    """
+    Fonction permettant d'attendre une saisie utilisateur pour continuer
+    :return: None
+    """
+
+    input("Appuyez sur Entrée pour continuer...")
+
+
+def find_pin_without_optimisation(ma_socket: socket.socket):
+    """
+    Fonction permettant de se connecter au site internet en testant tout les codes pin possibles
+    :param ma_socket: Socket utilisateur permettant de se connecter au site internet
+    :return:
+    """
+    timer_start = time.time()
+    pin_use = 0
+    pin_found = False
+    print('Recherche du code pin...')
+
+    for pin in range(1000):
+        print(pin)
+        ma_socket.sendall(bytes(str(pin).zfill(4), 'utf-8'))
+        ligne = str(ma_socket.recv(1024))
+        if not ligne == "b'Incorrect PIN\\n'":
+            pin_found = True
+            pin_use = pin
+            break
+
+    if not pin_found:
+        for pin in range(1000, 10000):
+            print(pin)
+            ma_socket.sendall(bytes(str(pin), 'utf-8'))
+            ligne = str(ma_socket.recv(1024))
+            if not ligne == "b'Incorrect PIN\\n'":
+                pin_use = pin
+                pin_found = True
+                break
+
+    if not pin_found:
+        print("PIN NOT FOUND (how ???)")
+    else:
+        print(f"PIN FOUND : {pin_use}")
+        timer_end = time.time()
+        print(f"Temps d'exécution : {round(timer_end - timer_start, 2)} secondes")
+
+
+def find_pin_with_a_sort(ma_socket: socket.socket):
+    """
+    Fonction permettant de se connecter au site internet en testant tout les codes pin possibles dans un ordre précis
+    grâce à un fichier csv
+    :param ma_socket: Socket utilisateur permettant de se connecter au site internet
+    :return:
+    """
+    timer_start = time.time()
+    pin_use = 0
+    pin_found = False
+
+    with open('four-digit-pin-codes-sorted-by-frequency-withcount.csv', newline='') as csvfile:
+        print('Recherche du code pin...')
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for pin, _ in spamreader:
+            ma_socket.sendall(bytes(pin, 'utf-8'))
+            ligne = str(ma_socket.recv(1024))
+            if not ligne == "b'Incorrect PIN\\n'":
+                pin_use = pin
+                pin_found = True
+                break
+
+        if not pin_found:
+            print("PIN NOT FOUND (how ???)")
+        else:
+            print(f"PIN FOUND : {pin_use}")
+            timer_end = time.time()
+            print(f"Temps d'exécution : {round(timer_end - timer_start, 2)} secondes")
+
+
 
 if __name__ == "__main__":
+    main_title = "Authentification à un site\nAppuyez sur les flèches pour naviguer et sur Entrée pour sélectionner"
+    main_options = ["Méthode sans trier les codes possibles", "Méthode avec un tri sur les codes possibles", None, "Quitter"]
+    main_menu = TerminalMenu(main_options, title=main_title, cycle_cursor=True, clear_screen=True)
     server = "51.195.253.124"
     port = 12345
     mode = 1
-    pin_found = False
 
     ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,37 +99,14 @@ if __name__ == "__main__":
     ligne = ma_socket.recv(1024)
     print(ligne)
 
-    if mode == 0:
-        with open('four-digit-pin-codes-sorted-by-frequency-withcount.csv', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',')
-            for pin, _ in spamreader:
-                ma_socket.sendall(bytes(pin, 'utf-8'))
-                ligne = str(ma_socket.recv(1024))
-                if not ligne == "b'Incorrect PIN\\n'":
-                    print(f"PIN FOUND : {pin}")
-                    print(ligne)
-                    pin_found = True
-                    break
-    else:
-        for i in range(1000):
-            ma_socket.sendall(bytes(str(i).zfill(4), 'utf-8'))
-            ligne = str(ma_socket.recv(1024))
-            if not ligne == "b'Incorrect PIN\\n'":
-                print(f"PIN FOUND : {i}")
-                print(ligne)
-                pin_found = True
-                break
-
-        if not pin_found:
-            for i in range(1000, 10000):
-                ma_socket.sendall(bytes(str(i), 'utf-8'))
-                ligne = str(ma_socket.recv(1024))
-                if not ligne == "b'Incorrect PIN\\n'":
-                    print(f"PIN FOUND : {i}")
-                    print(ligne)
-                    pin_found = True
-                    break
-
-    if not pin_found:
-        print("PIN NOT FOUND (how ???)")
-
+    while True:
+        main_entry_index = main_menu.show()
+        if main_entry_index == 0:
+            find_pin_without_optimisation(ma_socket)
+            await_input()
+        elif main_entry_index == 1:
+            find_pin_with_a_sort(ma_socket)
+            await_input()
+        elif main_entry_index == len(main_options) - 1 or main_entry_index is None:
+            print("Au revoir !")
+            exit(0)
