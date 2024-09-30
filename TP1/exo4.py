4567ef46e92c81fdb178b5fa43e14a14ae0485fb
Matteo NADLER CAMPOURCY <matteo.nadler.campou@gmail.com>
2024-09-30 11:16:14 +0200

    exo 4 : Réarrangement

--- TP1/exo4.py
+++ TP1/exo4.py
@@ -1,15 +1,24 @@
-import math
 from zipfile import ZipFile
 
 
-def get_alphabet():
+def get_alphabet() -> list:
+    """
+    Fonction pour récupérer les lettres de l'alphabet latin en minuscules
+    :return: la liste des lettres minuscules
+    """
     alphabet = []
     for i in range(26):
         alphabet.append(chr(ord('a') + i))
     return alphabet
 
 
-def get_passwords(size, pwd=""):
+def get_passwords(size, pwd="") -> str:
+    """
+    Fonction récursive pour générer les mots de passe de taille "size"
+    :param size: la taille des mots de passe
+    :param pwd: le mot de passe en cours de construction
+    :return: les mots de passe
+    """
     if size != 1:
         for char in get_alphabet():
             yield from get_passwords(size-1, pwd+char)
@@ -18,25 +27,31 @@ def get_passwords(size, pwd=""):
             yield pwd+char
 
 
-if __name__ == "__main__":
-    fileName = 'archive.zip'
-    with ZipFile(fileName) as fs:
+def archive_brute_force(filename: str) -> str:
+    """
+    Fonction pour brute force une archive zip protégée par mot de passe
+    :param filename: le nom de l'archive
+    :return: le mot de passe de l'archive
+    """
+    with ZipFile(filename) as fs:
         try:
             fs.extractall(pwd = bytes('', 'utf-8'))
         except RuntimeError as pwdRequired:
-            pwd_size = 1
-            pwd_found = False
-            while not pwd_found:
-                print(f"password trial size: {pwd_size}")
+            for i in range(6):
+                pwd_size = i + 1
+                print(f"Mot de passe essayé : {pwd_size}")
                 for password in get_passwords(pwd_size):
                     try:
                         fs.extractall(pwd = bytes(password, 'utf-8'))
                     except Exception as wrongPwd:
                         continue
-                    print(f"password is {password}")
-                    pwd_found = True
-                    break
-                pwd_size += 1
-                if pwd_size > 10:
-                    print("password not found...")
-                    pwd_found = True
+                    return password
+            return ""
+
+
+if __name__ == "__main__":
+    password = archive_brute_force('archive.zip')
+    if not password:
+        print("Aucun mot de passe trouvé")
+    else:
+        print("Mot de passe trouvé :", password)
